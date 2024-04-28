abaf037aa18d63919742070a80801ff7c6b4e038
Manish Dahiya <manishdahiya0944@gmail.com>
2024-04-28 09:53:21 +0530

    removed secrets

--- config/initializers/google_sign_in.rb
+++ config/initializers/google_sign_in.rb
@@ -1,4 +1,3 @@
 Rails.application.configure do
-  config.google_sign_in.client_id     = "560218744728-40rufnqdhe6bg9l4e5ttc4fnuibifqqg.apps.googleusercontent.com" 
-  config.google_sign_in.client_secret = "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"
+
 end
